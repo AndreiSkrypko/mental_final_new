a8b887b284c8533663656e48f00124094d0ff405
Andrei Skripko <skrip8078@gmail.com>
2025-08-20 06:55:59 +0300

    Initial commit without history

--- final_push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "D:\Cursor\mental_arifmetic"
-git add README.md
-git commit -m "Restore README.md without token"
-git push origin main
-echo README.md восстановлен и отправлен в репозиторий
-pause
